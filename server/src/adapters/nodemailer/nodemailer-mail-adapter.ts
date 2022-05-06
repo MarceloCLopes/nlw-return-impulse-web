321bbc4c24b0c0f9348dacc422ad60f13536db90
MarceloCLopes <marc01mcl@gmail.com>
2022-05-06 14:25:44 -0300

    Add project

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b0aece4e8c7062",
+    pass: "a1e0756dfe5432",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Marcelo Claro <marc01mcl@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
